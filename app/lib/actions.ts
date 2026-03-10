fe7703fb1fd8c6df5dfcf92a9666634921dcb81a
Jovana Sikanjic <jovana.sikanjic@leinfusion.com>
2026-03-10 10:01:11 +0100

    Tested validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,7 +1,8 @@
 'use server';
 
 import { z } from 'zod';
-
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 import postgres from 'postgres';
  
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
@@ -29,4 +30,6 @@ export async function createInvoice(formData: FormData) {
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
 }
