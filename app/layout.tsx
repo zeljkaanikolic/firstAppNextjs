832b57c2dbbfaf781d96871938b591727daf7efd
zeljkaanikolic <zeljka.nikolic@leinfusion.com>
2026-03-10 09:08:35 +0100

    added shadcn component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
 import '@/app/ui/global.css';
 import { inter } from 'app/ui/fonts';
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
