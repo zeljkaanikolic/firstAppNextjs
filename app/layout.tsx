6262a414e385c2661001e226fa56644ca7890afb
zeljkaanikolic <zeljka.nikolic@leinfusion.com>
2026-03-11 14:23:30 +0100

    Added handlers for errors and imporoved hci ascpect during filling out a forms

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={cn("font-sans", geist.variable)}>
+    <html  lang="en" className={cn("dark font-sans", geist.variable)}>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
